ebc8580a7c23c8bcfe36e4a44e678dfb6a8667e2
mrLuisFer <mrluisfeer@gmail.com>
2022-02-17 23:22:06 -0600

    add Colors class for colored terminal

--- Problems/CarsType.java
+++ Problems/CarsType.java
@@ -1,16 +1,17 @@
 package Problems;
 import java.util.Scanner;
 import Utils.Print;
+import Utils.Colors;
 
 public class CarsType {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		Print print = new Print();
 		
-		print.pr("Ingresa el tipo de vehiculo: bicicleta-'b' motocicleta-'m' auto-'a' trailer-'t'");
+		print.pr("Ingresa el tipo de vehiculo: bicicleta-'b' motocicleta-'m' auto-'a' trailer-'t'", Colors.CYAN);
 		String v = in.nextLine();
 		
-		print.pr("Ingresar la velocidad en Km:");
+		print.pr("Ingresar la velocidad en Km:", Colors.YELLOW);
 		double km = in.nextDouble();
 		
 		double p = 0;
@@ -19,18 +20,18 @@ public class CarsType {
 		} else if(v.equals("m") || v.equals("a")) {
 			p = (double)(km * 7.5);
 		} else if(v.equals("t")) {
-			print.pr("Ingresa toneladas de carga:");
+			print.pr("Ingresa toneladas de carga:", null);
 			int t = in.nextInt();
 			
 			p = (double)(t * 12 + km * 12.5);
 		} else {
-			print.pr("Vehiculo no valido");
+			print.pr("Vehiculo no valido", null);
 		}
 		
 		if (p > 0) {
-			print.pr("El valor total de tu " + v + " es de " + p);
+			print.pr("El valor total de tu " + v + " es de " + p, null);
 		} else {
-			print.pr("Algo salio mal");
+			print.pr("Algo salio mal", null);
 		}
 		
 		in.close();
--- Utils/Print.java
+++ Utils/Print.java
@@ -1,7 +1,11 @@
 package Utils;
 
 public class Print {
-	public <T> void pr(T any) {
-		System.out.println(any);
+	public <T> void pr(T any, String color) {
+		
+		if (color.equals(null)) color = Colors.RESET;
+		String txt = (String)(color + any);
+		System.out.println(txt);
+		System.out.println(Colors.RESET);
 	}
 }

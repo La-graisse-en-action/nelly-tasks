484d6e28368471f81546fd19c2f26ec9f5992bc1
mrLuisFer <mrluisfeer@gmail.com>
2022-03-17 01:48:42 -0600

    add solution for excercise 20

--- NellyTasks/Excercise20.java
+++ NellyTasks/Excercise20.java
@@ -1,5 +1,8 @@
 package NellyTasks;
 
+import java.util.Scanner;
+import java.io.IOException;
+
 /*
  * Alumnos:
  *	- Alvarez Manriquez Luis Fernando
@@ -7,14 +10,44 @@ package NellyTasks;
  * 
  * Problema 20:
  * Realizar un algoritmo que calcule y muestre el importe a pagar por un vehículo al circular por una autopista.
- * El vehículo puede ser  una  bicicleta( ́b ́),  una  moto( ́m ́),  un automóvil (‘a’)o  un  camión(‘c’).
+ * El vehículo puede ser  una  bicicleta( ́b ́),  una  moto( ́m ́),  un automóvil (‘a’) o  un  camión(‘c’).
  * El  importe  se  calculará  según  los siguientes datos:
- * a) * Un importe  de $1.5  por km para la bicicleta. 
- * b) Las motos y los automóvilespagarán $7.5 por Km.
- * c) Los camiones pagarán $27por Km. más $12por toneladade carga
+ * 
+ * a) Un importe  de $1.5  por km para la bicicleta. 
+ * b) Las motos y los automóviles pagarán $7.5 por Km.
+ * c) Los camiones pagarán $27 por Km. más $12 por tonelada de carga
  *
 */
 
 public class Excercise20 {
-
+	public static void main(String[] args) throws IOException {
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Ingresa vehiculo:");
+		char vehicle = (char) System.in.read();
+		System.in.read();
+		
+		System.out.println("Ingresar Kilometros:");
+		int km = (int) in.nextInt();
+		
+		double total;
+		double amount;
+		if(vehicle == 'b') {
+			amount = 1.5;
+			total = (double) (km * amount);
+			System.out.println("El total es: $" + total);
+		} else if(vehicle == 'm' || vehicle == 'a') {
+			amount = 7.5;
+			total = (double) (km * amount);
+			System.out.println("El total es: $" + total);
+		} else if(vehicle == 'c') {
+			System.out.println("Ingresa tolenadas de carga");
+			int tons = in.nextInt();
+			amount = 27;
+			total = (double) (km * amount) + (12 * tons);
+			System.out.println("El total es: $" + total);
+		} else System.err.println("Vehiculo no valido");
+		
+		in.close();
+	}
 }

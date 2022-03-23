eb91491b4e620f5b9795692d95bc86b7804b29b8
mrLuisFer <mrluisfeer@gmail.com>
2022-03-23 10:12:33 -0600

    starting problem 3

--- Unidad3/TareaCiclos/ActThree.java
+++ Unidad3/TareaCiclos/ActThree.java
@@ -1,8 +1,36 @@
 package Unidad3.TareaCiclos;
 
+import java.util.Scanner;
+
+/*
+ * Alumnos:
+ * - Alvarez Manriquez Luis Fernando
+ * -
+ * -
+ * 
+ * 3. Haz un algoritmo que calcule el resultado de dividir N/M, mediante restas sucesivas. 
+ * Considere que N y M son valores ingresados por el usuario y siempre recibirán valores mínimos de cero. 
+ * Mostrar cociente y resto de la división (prohibido utilizar los operadores /, * y mod). 
+ * Considera la división por cero como un error enviando un mensaje indicándolo.
+ */
+
 public class ActThree {
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
 		
+		Print.print("Ingresa el numerador: ");
+		int n = in.nextInt();
+		
+		Print.print("Ingresa denominador: ");
+		int m = in.nextInt();
+		
+		if(m != 0) {
+			
+		} else {
+			Print.println("No se puede dividir entre 0");
+		}
+		
+		in.close();
 	}
 	
 }

fc48e6cc1d7cbc793dc99a8cca7bf1a230e3f7fd
mrLuisFer <mrluisfeer@gmail.com>
2022-03-23 23:49:33 -0600

    fix details and indent of the estrella problem

--- Unidad3/TareaCiclos/EstrellaAct.java
+++ Unidad3/TareaCiclos/EstrellaAct.java
@@ -2,24 +2,21 @@ package Unidad3.TareaCiclos;
 import java.util.Scanner;
 
 public class EstrellaAct {
-
-	    public static void main(String[] args) {
-	        Scanner leer= new Scanner(System.in);
-	        double potencia=0;
-	        double suma=0;
-	        double mod;
-	        
-	        System.out.println("Ingrese su valor binario (solo ceros (0) y unos(1)");
-	        int valor= leer.nextInt();
-	        while(valor>0){
-	            mod=valor%10;
-	            mod=mod*2;
-	            mod = Math.pow(mod,potencia);
-	            suma=suma+mod;
-	            valor=valor/10;
-	            potencia++;
-	        }
-	       
-	         System.out.println("La suma de potencias es "+suma);
-	    }
+	public static void main(String[] args) {
+		Scanner leer = new Scanner(System.in);
+		double potencia = 0 ,suma = 0, mod;
+	    System.out.print("Ingrese su valor binario (solo ceros (0) y unos(1): ");
+	    int valor= leer.nextInt();
+	    while(valor > 0){
+	    	mod = valor % 10;
+	        mod *= 2;
+	        mod = Math.pow(mod,potencia);
+	        suma += mod;
+	        valor /= 10;
+	        potencia++;
+	        Print.ln(suma);
+	    }   
+	    System.out.println("La suma de potencias es " + ((int) suma - 1));
+	    leer.close();
+	}
 }

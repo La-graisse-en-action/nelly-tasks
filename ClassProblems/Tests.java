78a17b1f8000fe1be8572e4230e475af4c6ef0d1
mrLuisFer <mrluisfeer@gmail.com>
2022-03-26 23:43:20 -0600

    create classProblems

---
+++ ClassProblems/Tests.java
@@ -0,0 +1,43 @@
+package ClassProblems;
+
+import Unidad3.TareaCiclos.Print;
+import java.util.*;
+
+public class Tests {
+	public static void main(String[] args) {
+		/*
+		// Convertir de un numero a un String
+		int x = 5;
+		String y = String.valueOf(x);
+		// Print.ln(y);
+		
+		// Convertir de texto a double
+		String cad = "7.23";
+		double u = Double.parseDouble(cad); // Se utiliza el parse<Type>() para convertir el tipo
+		// Print.ln(u);
+		
+		// Cualquier cosa que no sea boolean al parsearlo será false
+		boolean val = Boolean.parseBoolean(cad);
+		// Print.ln(val);
+		
+		// Math Class
+		// Print.ln((int) Math.pow(2, 4)); // .pow() retorna un double
+		
+		Random ran = new Random();
+		
+		int p;
+		/* do {		
+			p = (int)(ran.nextInt(50) * Math.random());
+			Print.ln(p);
+		} while(p != 0);
+		for(int i = 0; i <= 10; i++) {
+			p = (int)(ran.nextInt(50) * Math.random());
+			Print.ln(p);
+		}
+		
+		// ran.nextInt(21) + 30; * Sumamos el minimo y lo que resta dentro de los parentesis
+		*/
+		
+		
+	}
+}

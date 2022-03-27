c7f1b4cd03bcbc57b21519ca8da6a4c8b51267d1
mrLuisFer <mrluisfeer@gmail.com>
2022-03-27 16:17:58 -0600

    first version of excercise 4

--- Unidad3/MasTareaDeCiclos/ActFour.java
+++ Unidad3/MasTareaDeCiclos/ActFour.java
@@ -2,34 +2,52 @@ package Unidad3.MasTareaDeCiclos;
 
 import java.util.Scanner;
 
+import Unidad3.TareaCiclos.Print;
+
 /**
  * 
  * @author Alvarez Manriquez Luis Fernando
  * @author
  * @author
  *
- * 4. Introduce un par de cadenas repetidamente. Del par ingresado, indique cuál de los dos es más largo. 
- * El programa termina cuando ambas cadenas tienen la misma longitud. 
- * Ejemplo: 
- * Ingrese una cadena: Morelia 
- * Ingrese otra cadena: cerdo 
- * La cadena más larga es Morelia 
+ *         4. Introduce un par de cadenas repetidamente. Del par ingresado,
+ *         indique cuál de los dos es más largo. El programa termina cuando
+ *         ambas cadenas tienen la misma longitud.
  * 
- * Ingrese una cadena: sí 
- * Ingrese otra cadena: claro 
- * La cadena más larga es clara 
+ *         Ejemplo: Ingrese una cadena: Morelia Ingrese otra cadena: cerdo La
+ *         cadena más larga es Morelia
  * 
- * Ingrese una cadena: sí 
- * Ingrese otra cadena: no 
- * FIN
+ *         Ingrese una cadena: sí Ingrese otra cadena: claro La cadena más larga
+ *         es clara
+ * 
+ *         Ingrese una cadena: sí Ingrese otra cadena: no FIN
  * 
  */
 public class ActFour {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		
-		String cad = in.next();
-		
+
+		Print.print("Ingrese una cadena: ");
+		String cad = in.next().toLowerCase();
+
+		Print.print("Ingrese otra cadena: ");
+		String cad2 = in.next().toLowerCase();
+
+		while (cad2.length() != cad.length()) {
+			String val;
+			if (cad.length() > cad2.length())
+				val = cad;
+			else
+				val = cad2;
+			Print.ln("La cadena mas larga es: " + val + "\n");
+
+			Print.print("Ingrese una cadena: ");
+			cad = in.next().toLowerCase();
+
+			Print.print("Ingrese otra cadena: ");
+			cad2 = in.next().toLowerCase();
+		}
+		Print.ln("FIN");
 		in.close();
 	}
 }

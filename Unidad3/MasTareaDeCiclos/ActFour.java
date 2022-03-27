e6ef3afd2a20effb3a2f4fae31ab337c58d773c7
mrLuisFer <mrluisfeer@gmail.com>
2022-03-27 17:33:34 -0600

    format code

--- Unidad3/MasTareaDeCiclos/ActFour.java
+++ Unidad3/MasTareaDeCiclos/ActFour.java
@@ -1,27 +1,17 @@
 package Unidad3.MasTareaDeCiclos;
-
 import java.util.Scanner;
-
 import Unidad3.TareaCiclos.Print;
 
 /**
- * 
  * @author Alvarez Manriquez Luis Fernando
- * @author
- * @author
- *
- *         4. Introduce un par de cadenas repetidamente. Del par ingresado,
- *         indique cuál de los dos es más largo. El programa termina cuando
- *         ambas cadenas tienen la misma longitud.
- * 
- *         Ejemplo: Ingrese una cadena: Morelia Ingrese otra cadena: cerdo La
- *         cadena más larga es Morelia
- * 
- *         Ingrese una cadena: sí Ingrese otra cadena: claro La cadena más larga
- *         es clara
- * 
- *         Ingrese una cadena: sí Ingrese otra cadena: no FIN
- * 
+ * @author Jesus Salvador Mora González
+ * @author Irma Estrella Rivera Durán 4. Introduce un par de cadenas
+ *         repetidamente. Del par ingresado, indique cuál de los dos es más
+ *         largo. El programa termina cuando ambas cadenas tienen la misma
+ *         longitud. Ejemplo: Ingrese una cadena: Morelia Ingrese otra cadena:
+ *         cerdo La cadena más larga es Morelia Ingrese una cadena: sí Ingrese
+ *         otra cadena: claro La cadena más larga es clara Ingrese una cadena:
+ *         sí Ingrese otra cadena: no FIN
  */
 public class ActFour {
 	public static void main(String[] args) {
--- Unidad3/MasTareaDeCiclos/ActOne.java
+++ Unidad3/MasTareaDeCiclos/ActOne.java
@@ -4,42 +4,36 @@ import java.util.Scanner;
 import Unidad3.TareaCiclos.Print;
 
 /**
- * 
  * @author Alvarez Manriquez Luis Fernando
- * @author 
- * @author 
- * 
- * 1. Ingrese una cadena e imprima la subcadena que resulta de la tercera vocal de la cadena, 
- * o muestre el mensaje "La cadena no tiene 3 vocales". 
- * Ejemplos: 
- * Introducir cadena: murcielago 
- * La subcadena de la tercera vocal: elago 
- * 
- * Introducir cadena: corazón 
- * La subcadena de la tercera voz: on
- * 
- * Introducir cadena:oso
- * La cadena no tiene 3 vocales
- *
+ * @author Jesus Salvador Mora González
+ * @author Irma Estrella Rivera Durán 1. 
+ * 		   Ingrese una cadena e imprima la
+ *         subcadena que resulta de la tercera vocal de la cadena, o muestre el
+ *         mensaje "La cadena no tiene 3 vocales". Ejemplos: Introducir cadena:
+ *         murcielago La subcadena de la tercera vocal: elago Introducir cadena:
+ *         corazón La subcadena de la tercera voz: on Introducir cadena:oso La
+ *         cadena no tiene 3 vocales
  */
 public class ActOne {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		
+
 		Print.print("Ingrese una cadena de texto: ");
 		String cad = in.next().toLowerCase();
-		
+
 		String z = "";
 		char c = 0;
 		int cont = 0, i;
 		for (i = 0; i < cad.length() && cont < 3; i++) {
 			z = cad.substring(i);
 			c = z.charAt(0);
-			if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') cont++;			
+			if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+				cont++;
 		}
-		if(cont == 3) Print.ln(z);			
-		else Print.error("La palabra " + cad + " no tiene 3 vocales");
-		
-		in.close();	
+		if (cont == 3)
+			Print.ln(z);
+		else
+			Print.error("La palabra " + cad + " no tiene 3 vocales");
+		in.close();
 	}
 }

02d86e0614ee1feda6daa173bd46e7228e663a41
mrLuisFer <mrluisfeer@gmail.com>
2022-03-27 01:20:26 -0600

    create excercise four

---
+++ Unidad3/MasTareaDeCiclos/ActFour.java
@@ -0,0 +1,35 @@
+package Unidad3.MasTareaDeCiclos;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * @author Alvarez Manriquez Luis Fernando
+ * @author
+ * @author
+ *
+ * 4. Introduce un par de cadenas repetidamente. Del par ingresado, indique cuál de los dos es más largo. 
+ * El programa termina cuando ambas cadenas tienen la misma longitud. 
+ * Ejemplo: 
+ * Ingrese una cadena: Morelia 
+ * Ingrese otra cadena: cerdo 
+ * La cadena más larga es Morelia 
+ * 
+ * Ingrese una cadena: sí 
+ * Ingrese otra cadena: claro 
+ * La cadena más larga es clara 
+ * 
+ * Ingrese una cadena: sí 
+ * Ingrese otra cadena: no 
+ * FIN
+ * 
+ */
+public class ActFour {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		String cad = in.next();
+		
+		in.close();
+	}
+}

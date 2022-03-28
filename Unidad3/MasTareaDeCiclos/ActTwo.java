58c0926ad42927202398063b7b71041f3be373b9
mrLuisFer <mrluisfeer@gmail.com>
2022-03-27 19:32:56 -0600

    add excercise two by Estrella Rivera

---
+++ Unidad3/MasTareaDeCiclos/ActTwo.java
@@ -0,0 +1,24 @@
+package Unidad3.MasTareaDeCiclos;
+
+import java.util.Scanner;
+
+public class ActTwo {
+	// By Estrella Rivera
+	public static void main(String[] args) {
+		Scanner leer = new Scanner(System.in);
+		String cad, cad2 = "";
+		boolean b = false;
+		while (b == false) {
+			System.out.print("Ingrese cadena: ");
+			cad = leer.next().trim();
+			if (cad.equals("parar"))
+				b = true;
+			else
+				cad2 += cad + " ";
+		} // while
+
+		System.out.println(cad2.toUpperCase());
+		leer.close();
+	}// static void
+
+}

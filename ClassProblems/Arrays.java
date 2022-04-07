24f80682f13609a375dd3cf0fe5dc49476e4a016
mrLuisFer <mrluisfeer@gmail.com>
2022-04-07 01:02:09 -0500

    add some tests

--- ClassProblems/Arrays.java
+++ ClassProblems/Arrays.java
@@ -1,7 +1,12 @@
 package ClassProblems;
 
+import Unidad3.TareaCiclos.Print;
+
 public class Arrays {
 	public static void main(String[] args) {
 		String[] arr = {"Hello", "World"};
+		
+		int matriz [] [] = {{1,3},{3,4}};
+		Print.ln("");
 	}
 }
--- ClassProblems/Tests.java
+++ ClassProblems/Tests.java
@@ -1,43 +1,20 @@
 package ClassProblems;
 
 import Unidad3.TareaCiclos.Print;
-import java.util.*;
+import java.util.Scanner;
 
 public class Tests {
 	public static void main(String[] args) {
-		/*
-		// Convertir de un numero a un String
-		int x = 5;
-		String y = String.valueOf(x);
-		// Print.ln(y);
+		Scanner sc = new Scanner(System.in);
+		Print.ln("Ingresa un numero: ");
+		int n = sc.nextInt();
 		
-		// Convertir de texto a double
-		String cad = "7.23";
-		double u = Double.parseDouble(cad); // Se utiliza el parse<Type>() para convertir el tipo
-		// Print.ln(u);
-		
-		// Cualquier cosa que no sea boolean al parsearlo será false
-		boolean val = Boolean.parseBoolean(cad);
-		// Print.ln(val);
-		
-		// Math Class
-		// Print.ln((int) Math.pow(2, 4)); // .pow() retorna un double
-		
-		Random ran = new Random();
-		
-		int p;
-		/* do {		
-			p = (int)(ran.nextInt(50) * Math.random());
-			Print.ln(p);
-		} while(p != 0);
-		for(int i = 0; i <= 10; i++) {
-			p = (int)(ran.nextInt(50) * Math.random());
-			Print.ln(p);
+		int x = 0;
+		for(int i = 1; i <= n; i++) {
+			x += i;
 		}
+		Print.ln("Resultado: " + x);
 		
-		// ran.nextInt(21) + 30; * Sumamos el minimo y lo que resta dentro de los parentesis
-		*/
-		
-		
+		sc.close();
 	}
 }

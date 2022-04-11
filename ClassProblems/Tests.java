99d5db2e0325e699c6432b48d0a0ae66ceaf8151
mrLuisFer <mrluisfeer@gmail.com>
2022-04-10 22:58:02 -0500

    remove unnecessary files

--- ClassProblems/Tests.java
+++ ClassProblems/Tests.java
@@ -5,16 +5,9 @@ import java.util.Scanner;
 
 public class Tests {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		Print.ln("Ingresa un numero: ");
-		int n = sc.nextInt();
-		
-		int x = 0;
-		for(int i = 1; i <= n; i++) {
-			x += i;
-		}
-		Print.ln("Resultado: " + x);
-		
-		sc.close();
+		int x = 5;
+		int y = 5;
+		y /= ++x;
+		Print.ln(y);
 	}
 }
